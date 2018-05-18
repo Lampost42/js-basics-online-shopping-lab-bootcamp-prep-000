3edbe5c54dccf333288a89eaa577bd5f19f4f803
Trevor Russell <truss.42@gmail.com>
2018-05-18 18:24:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function placeOrder(cardNumber) {
   if(isFinite(cardNumber) === true) {
     var newTotal = total();
     cart = [];
-    return(`Your total cost is ${newTotal}, which will be charged to the card ${cardNumber}.`)
+    return(`Your total cost is $${newTotal}, which will be charged to the card ${cardNumber}.`)
   } else {
     return('Sorry, we don\'t have a credit card on file for you.')
   }
