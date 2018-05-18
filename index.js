832425f25596edc4a78933f8eace0a2178c3373d
Trevor Russell <truss.42@gmail.com>
2018-05-18 18:13:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,48 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {
+  "itemName": item,
+  "itemPrice": Math.floor(Math.random() * 100) + 1
+  };
+  cart.push(newItem);
+  return (`${item} has been added to your cart.`)
 }
 
 function viewCart() {
-  // write your code here
+  var list = [];
+  if(cart.length === 0) {
+    return ('Your shopping cart is empty.')
+  } else if(cart.length === 1) {
+  list.push(` ${cart[0].itemName} at $${cart[0].itemPrice}.`);
+      return(`In your cart, you have${list}`)
+  } else{
+    for (var i = 0; i < cart.length; i++) {
+      if (i < cart.length - 1) {
+        list.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`);
+      } else {
+        list.push(` and ${cart[i].itemName} at $${cart[i].itemPrice}.`);
+      }
+    }
+    return(`In your cart, you have${list}`)
+}
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    total += parseInt(cart[i].itemPrice );
+  } 
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var index = cart.indexOf(item);
+  if(index != -1) {
+    cart.splice(index, 1)
+  } else {
+    return('That item is not in your cart.')
+  }
 }
 
 function placeOrder(cardNumber) {
